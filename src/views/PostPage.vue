ddd72b0f288cf27fadbd5688c78f9f24c9e534fc
zanchess <a.lashkevich93@gmail.com>
2024-01-17 13:16:54 +0300

    feat: refactored options api to composition api

--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -1,40 +1,43 @@
-<!-- PostPage.vue -->
 <template>
-  <div  class="card-page">
-    <Post
-    v-for="post in posts"
-    :key="post.id"
-    :title="post.title"
-    :body="post.body"
-    :user-id="post.userId"
+  <div v-if="isLoading" :class="{ 'centered-spinner': isLoading }">
+    <atom-spinner v-if="isLoading" :animation-duration="1000" :size="60" color="#747bff" />
+  </div>
+  <div v-else class="post-page">
+    <Post v-for="post in posts"
+          :key="post.id" :post="post"
+          :title="post.title"
+          :body="post.body"
+          :user-id="post.userId"
     />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, computed } from "vue";
-import { usePostStore } from "../stores/postStore.ts";
-import Post from "../components/Post.vue";
+<script setup lang="ts">
+import { computed, onMounted } from 'vue';
+import { usePostStore } from '../stores/postStore.ts';
+import Post from '../components/Post.vue';
+import { AtomSpinner } from 'epic-spinners';
 
-export default defineComponent({
-  components: {
-    Post,
-  },
-  setup() {
-    const postStore = usePostStore();
-    const posts = computed(() => postStore.posts);
+const postStore = usePostStore();
 
-    return {
-      posts,
-    };
-  },
-  mounted() {
-    const { fetchPosts } = usePostStore();
-    fetchPosts();
-  },
+const posts = computed(() => postStore.posts);
+const isLoading = computed(() => postStore.isLoading);
+
+onMounted(() => {
+  postStore.fetchPosts();
 });
 </script>
 
 <style scoped>
-/* You can add styles for your card here */
+.post-page {
+  margin: 20px;
+}
+
+.centered-spinner {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
 </style>
